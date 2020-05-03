92a9e032725f2d6dde91375cac6df61897699307
ejones11 <64102591+ejones11@users.noreply.github.com>
2020-05-02 23:40:50 -0400

    Update Getting Player Stats (1).py

--- Getting Player Stats (1).py
+++ Getting Player Stats (1).py
@@ -16,6 +16,7 @@ for player in data:
     if player['YahooName'].lower() == enter.lower():
         print(player['YahooName'], player['PlayerID'])
    
+
     
 
     
@@ -68,6 +69,32 @@ data
 data_df = pd.DataFrame(data)
 info = data_df[ ['FirstName','LastName','Height','Team','Position','Jersey','College','Salary'] ]
 info
+
+Player Bios
+import pandas as pd
+import requests
+enter=input("Enter a player")
+key='a135d562f81946ae8aa641cb331294a0'
+
+url='https://api.sportsdata.io/v3/nba/scores/json/Players'
+params={'key':key}
+response= requests.get(url,params)
+data=response.json()
+for player in data:
+    if player['YahooName'].lower() == enter.lower():
+        playerid= player['PlayerID']
+url=f'https://api.sportsdata.io/v3/nba/scores/json/Player/{playerid}'
+params={'key':key}
+response=requests.get(url,params)
+data=response.json()
+
+#Reference from my Code 
+#stats=[]
+#stats.append({'player':data['Name'],'G':data['Games'],'Points':data['Points'],'FGA':data['FieldGoalsAttempted'],'FGM':data['FieldGoalsMade'],'FG%':data['FieldGoalsPercentage'], 'EFG%':data['EffectiveFieldGoalsPercentage'], 'FTA':data['FreeThrowsAttempted'], 'FTM':data['FreeThrowsMade'],'FT%':data['FreeThrowsPercentage'], 'ORB':data['OffensiveRebounds'], 'DRB': data['DefensiveRebounds'], 'TRB': data['Rebounds'], 'AST':data['Assists'], 'TOV': data['Turnovers'], 'STL':data['Steals'], 'BLK': data['BlockedShots'], 'PF':data['PersonalFouls'],'+/-':data['PlusMinus'], 'PER':data['PlayerEfficiencyRating']})
+#stats_data=pd.DataFrame(stats)
+#stats_data
+
+
 
 
 
